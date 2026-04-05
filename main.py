fd9392b322735e6b138ee9856fec385d97bc63e3
группы стационара <st@itstep.lan>
2026-04-05 17:32:59 +0300

    first commit

--- main.py
+++ main.py
@@ -2,12 +2,43 @@ import pygame
 
 pygame.init()
 
-start_position = pygame.Vector2(3,4)
-destination = pygame.Vector2(8,7)
+
+
+
+
+pygame.init()
+
+FPS = 60
+
+SCREEN_WIDTH =1000
+SCREEN_HEIGHT =800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+
+running = True
+
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+destination = pygame.Vector2(screen.get_width()/3,screen.get_height()/3)
 destination_2= pygame.Vector2(9,1)
-wey_lenght = start_position.distance_to(destination)
-wey_lenght2= destination.distance_to(destination_2)
-wey_lenght3= destination_2.distance_to(start_position)
-print(wey_lenght)
-print(wey_lenght2)
-print(wey_lenght3)
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    player_pos += dt*300*(player_pos-destination).normalize()
+
+
+
+    pygame.display.flip()
+
+
+    dt = clock.tick(FPS) / 1000
+
+pygame.quit()
